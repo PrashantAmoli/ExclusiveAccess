f153465caf7451ff8d1845701c6b11f164ff7ee8
Prashant <prashantamoli2621@gmail.com>
2023-11-08 10:45:26 +0530

    Update Analytics import in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import { Toaster } from "sonner";
-import { Analytics } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
