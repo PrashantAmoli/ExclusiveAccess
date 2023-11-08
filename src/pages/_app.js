f0c4aee59900ec953b768bc72c9c1386a3f269c1
Prashant <prashantamoli2621@gmail.com>
2023-11-08 10:39:23 +0530

    Added @vercel/analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,15 @@
-import '@/styles/globals.css'
-import { Toaster } from 'sonner';
+import "@/styles/globals.css";
+import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
 
-      <Toaster position='bottom-center' />
+      <Toaster position="bottom-center" />
+
+      <Analytics />
     </>
-  )
+  );
 }
